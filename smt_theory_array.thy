9ccad0b746b12faed4cf057695e46bb609718c04
Peter Zeller <p_zeller@cs.uni-kl.de>
2020-03-30 00:33:27 +0200

    array axioms

--- smt_theory_array.thy
+++ smt_theory_array.thy
@@ -16,14 +16,19 @@ definition array_sig :: signature where
 definition is_array_model :: "'u::universe interpr \<Rightarrow> bool" where
 "is_array_model I \<equiv> 
   \<forall>s1 s2.
-  let arrays = i_elements I (CType t_array [k, v])
+  let arrays = i_elements I (CType t_array [s1, s2])
   in 
-  (\<forall>a\<in>arrays. \<forall>i\<in>i_elemens I s1. \<forall>e\<in>i_elements I s2.
-    Apply c_select [Apply c_store [a, i, e]])
-
+  (\<forall>a\<in>arrays. \<forall>i\<in>i_elements I s1. \<forall>e\<in>i_elements I s2.
+    i_evaluate_const I c_select [i_evaluate_const I c_store [a, i, e], i] = e)
+  \<and> (\<forall>a\<in>arrays. \<forall>i\<in>i_elements I s1. \<forall>j\<in>i_elements I s1. \<forall>e\<in>i_elements I s2.
+      i \<noteq> j \<longrightarrow> (i_evaluate_const I c_select [i_evaluate_const I c_store [a, i, e], j])
+                = i_evaluate_const I c_select [a, j])
+  \<and> (\<forall>a\<in>arrays. \<forall>b\<in>arrays. 
+      (\<forall>i\<in>i_elements I s1. i_evaluate_const I c_select [a, i] = i_evaluate_const I c_select [b, i]
+        \<longrightarrow> a = b))
 "
 
-definition array_theory :: "'u smt_theory" where
+definition array_theory :: "'u::universe smt_theory" where
 "array_theory \<equiv> \<lparr>
   t_signature = array_sig,
   t_models = Collect is_array_model
